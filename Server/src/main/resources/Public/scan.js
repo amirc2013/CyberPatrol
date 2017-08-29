01a5feef1af0a76b4a08ad7bdec8eafcef3f6010
Niv Shalmon <shalmon.niv@gmail.com>
2017-08-29 06:44:40 +0300

    making things prettier

--- Server/src/main/resources/Public/scan.js
+++ Server/src/main/resources/Public/scan.js
@@ -1,12 +1,50 @@
+/**
+* Parses get params out of an url
+* @param string url - an url . if empty - parses current page's url
+* @return array GET - an array of params passed
+*/
+function parseGET(url)
+{  
+  if(!url || url == '') url = document.location.search;
+  if(url.indexOf('?') < 0) return Array();
+
+  url = url.split('?');
+  url = url[1];
+  
+  var GET = [];
+  var params = [];
+  var keyval = [];
+
+  if(url.indexOf('#')!=-1)    
+  {    
+    anchor = url.substr(url.indexOf('#')+1); 
+    url = url.substr(0,url.indexOf('#'));
+  }
+
+  if(url.indexOf('&') > -1) params = url.split('&');
+  else params[0] = url;
+
+  for (i=0; i<params.length; i++)
+  {
+    if(params[i].indexOf('=') > -1) keyval = params[i].split('=');
+    else { keyval[0] = params[i]; keyval[1] = true; }
+    GET[keyval[0]]=keyval[1];
+  }
+     
+  return (GET); 
+};
+
+/**
+* the andular module
+*/
 angular.module('scanApp', [])
 	.controller('scanControler', function($http) {
 		var db = this;
-		db.site = {url:'url',classifictclassification:'in progress'};
-		$http({
-		  method: 'GET',
-		  url: 'http://192.168.99.203:4567/getWebsites'
-		}).then(function successCallback(response) {
-			db.site = response.data[0];
+		var $_GET = parseGET()
+		var scan_url = $_GET['url']
+		db.site = {url:scan_url,classifictclassification:'בבדיקה'};
+		$http.post('/url',scan_url,'').then(function successCallback(response) {
+			db.site = response.data;
 		  }, function errorCallback(response) {
 			// called asynchronously if an error occurs
 			// or server returns response with an error status.
